5d7a9bfeceeba47167c88a9e02fff7d168a633ec
Fletcher <fletcher.hart@mymail.champlain.edu>
2018-12-27 14:00:14 -0500

    Separate pages for images
    Can now go to different pages to see different content.

--- sample.php
+++ sample.php
@@ -10,8 +10,9 @@
         <input type="file" name="fileToUpload" id="fileToUpload">
         <input type="submit" value="Upload Image" name="submit">
     </form>
+    
 
-    <div class="row">
+    <div id="row">
             <?php     
                 require '../../configure.php';
                 
@@ -21,54 +22,32 @@
                 
                 $db_found = mysqli_select_db( $db_handle, $database );
                 
-                $SQL = "SELECT * FROM tbl_images";
+                $start = intval($_GET['page'])*3;
+                
+                $SQL = "SELECT * FROM tbl_images WHERE ID LIMIT ".$start.",3";
                 
                 $result = mysqli_query($db_handle, $SQL);
                 
                 
                 while ( $db_field = mysqli_fetch_assoc($result) ) {
-                    echo '<div ><img class="images" src="'.$db_field['fileName'].'" style="width:100;height:100" /><br /></div>';
-                    
+                    echo '<div><img class="images" src="'.$db_field['fileName'].'" /><br /></div>';
                 }
                 
+                $SQL = "SELECT * FROM tbl_images";
+                $result = mysqli_query($db_handle, $SQL);
+                $num_row = mysqli_num_rows($result);
+                
+                
                 mysqli_close($db_handle);
+                echo '<div id="num">';
+                for($x = 0; $x <= $num_row/3; $x++){
+                    echo '<a href="sample.php?page='.$x.'">'.($x+1).'</a>';
+                }
+                echo '</div>';
             ?>
+            
+            
     </div>
-
-    <?php 
-        /*
-        require '../configure.php';
-        
-        $db_handle = mysqli_connect(DB_SERVER,DB_USER,DB_PASS);
-        
-        $database="addressbook";
-        
-        $db_found = mysqli_select_db( $db_handle, $database );
-        
-        if($db_found){            
-            $SQL = "INSERT INTO images (fileName) VALUES ()";
-            $result = mysqli_query($db_handle, $SQL);
-            
-           // $SQL = "SELECT * FROM tbl_address_book";
-           // $result = mysqli_query($db_handle, $SQL);
-            
-            while ( $db_field = mysqli_fetch_assoc($result) ) {
-
-            print $db_field['ID'] . "<BR>";
-            print $db_field['First_Name'] . "<BR>";
-            print $db_field['Surname'] . "<BR>";
-            print $db_field['Address'] . "<BR>";
-            }
-            
-          
-        }
-        else{
-            print "Database not found";
-        }
-        
-        mysqli_close($db_handle);
-        */
-    ?>
 
 </body>
 </html>
