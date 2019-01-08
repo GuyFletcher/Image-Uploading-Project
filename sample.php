26fea18e654586eb8216ab4115a865daaa4764e1
Fletcher <fletcher.hart@mymail.champlain.edu>
2019-01-08 13:58:36 -0500

    Removed redundant file + CSS work
    Removed search.php and added functionality to sample.php. Changed logic for page numbers + added styling. Put images inside grid display.

--- sample.php
+++ sample.php
@@ -16,7 +16,7 @@
     
     <div id="row">
         <div id="column1">
-            <form action="search.php">
+            <form action="sample.php">
                 Search:
                 <input type="text" name="search">
                 <input type="submit">
@@ -38,49 +38,99 @@
                 
                 $database="images";
                 
-                $numImages = 5; //number of images per page
+                $numImages = 4; //number of images per page
                 
                 $db_found = mysqli_select_db( $db_handle, $database );
                 
-                if(isset($_GET['page'])){
-                   $start = intval($_GET['page'])* $numImages;    //if page is set, multiply by numImages for starting value 
-                }
-                else {
-                    header("Refresh:0; url=sample.php?page=0");     //refresh and set page=0
-                }
-                
-                
-                $SQL = "SELECT * FROM tbl_images WHERE ID LIMIT ".$start.",".$numImages."";
-                
-                $result = mysqli_query($db_handle, $SQL);
-                
-                $img_count = 0;
-                echo '<div class="imgRow">';
-                while ( $db_field = mysqli_fetch_assoc($result) ) {
-                    if($img_count < 4){
-                        echo '<a href="image.php?image='.$db_field['fileName'].'"><img class="images" src="uploads/'.$db_field['fileName'].'"/></a><br />';
+                if(isset($_GET['search'])) {
+                    
+                    if(isset($_GET['page'])){
+                        $start = intval($_GET['page'])* $numImages;    //if page is set, multiply by numImages for starting value 
                     }
                     else {
-                        echo '</div>';
-                        $img_count = 1;
-                        echo '<div class="imgRow">';
-                        echo '<a href="image.php?image='.$db_field['fileName'].'"><img class="images" src="uploads/'.$db_field['fileName'].'"/></a><br />';
+                        header("Refresh:0; url=sample.php?search=".$_GET['search']."&page=0");
                     }
-                    $img_count+=1;
+                    
+                    
+                    $SQL = "SELECT tbl_images.fileName FROM tbl_images 
+                    INNER JOIN tbl_img_tags ON tbl_images.ID = tbl_img_tags.IMG_ID 
+                    INNER JOIN tbl_tags ON tbl_img_tags.TAG_ID = tbl_tags.ID WHERE tbl_tags.tag = '".$_GET['search']."' LIMIT ".$start.",".$numImages."";
+                    
+                    $result = mysqli_query($db_handle, $SQL);
+                    
+                    echo '<div class="imgRow">';
+                    while ( $db_field = mysqli_fetch_assoc($result) ) {
+                        echo '<a href="image.php?image='.$db_field['fileName'].'"><img class="images" src="uploads/'.$db_field['fileName'].'"/></a>';
+                    }
+                    echo '</div>';
+                    
+                    $SQL = "SELECT tbl_images.fileName FROM tbl_images 
+                    INNER JOIN tbl_img_tags ON tbl_images.ID = tbl_img_tags.IMG_ID 
+                    INNER JOIN tbl_tags ON tbl_img_tags.TAG_ID = tbl_tags.ID WHERE tbl_tags.tag = '".$_GET['search']."'";
+                    $result = mysqli_query($db_handle, $SQL);
+                    $num_row = mysqli_num_rows($result);
+                    
+                    
+                    mysqli_close($db_handle);
+                    echo '<div id="num">';
+                    for($x = 0; $x < $num_row/$numImages; $x++){
+                        
+                        if($x > $_GET['page']+2 AND $x != $num_row/$numImages-1){
+                                echo "...";
+                                $x = $num_row/$numImages-1;
+                                echo '<a class="pgNum" href="sample.php?search='.$_GET['search'].'&page='.$x.'">'.($x+1).'</a>';
+                            }
+                            else if ($x < $_GET['page']-2 AND $x != 0) {
+                                echo "...";
+                            }
+                            else {
+                                echo '<a class="pgNum" href="sample.php?search='.$_GET['search'].'&page='.$x.'">'.($x+1).'</a>';
+                            }
+                    }
+                    echo '</div>';                    
                 }
-                echo '</div>';
-                
-                $SQL = "SELECT * FROM tbl_images";
-                $result = mysqli_query($db_handle, $SQL);
-                $num_row = mysqli_num_rows($result);
-                
-                
-                mysqli_close($db_handle);
-                echo '<div id="num">';
-                for($x = 0; $x < $num_row/$numImages; $x++){
-                    echo '<a href="sample.php?page='.$x.'">'.($x+1).'</a>';
+                else {
+                    if(isset($_GET['page'])){
+                       $start = intval($_GET['page'])* $numImages;    //if page is set, multiply by numImages for starting value 
+                    }
+                    else {
+                        header("Refresh:0; url=sample.php?page=0");     //refresh and set page=0
+                    }
+                    
+                    
+                    $SQL = "SELECT * FROM tbl_images WHERE ID LIMIT ".$start.",".$numImages."";
+                    
+                    $result = mysqli_query($db_handle, $SQL);
+                    
+                    echo '<div class="imgRow">';
+                    while ( $db_field = mysqli_fetch_assoc($result) ) {
+                        echo '<a href="image.php?image='.$db_field['fileName'].'"><img class="images" src="uploads/'.$db_field['fileName'].'"/></a>';
+                    }
+                    echo '</div>';
+                    
+                    $SQL = "SELECT * FROM tbl_images";
+                    $result = mysqli_query($db_handle, $SQL);
+                    $num_row = mysqli_num_rows($result);
+                    
+                    
+                    mysqli_close($db_handle);
+                    echo '<div id="num">';
+                    for($x = 0; $x < $num_row/$numImages; $x++){
+                        
+                        if($x > $_GET['page']+2 AND $x != $num_row/$numImages-1){
+                            echo "...";
+                            $x = $num_row/$numImages-1;
+                            echo '<a class="pgNum" href="sample.php?page='.$x.'">'.($x+1).'</a>';
+                        }
+                        else if ($x < $_GET['page']-2 AND $x != 0) {
+                            echo "...";
+                        }
+                        else {
+                            echo '<a class="pgNum" href="sample.php?page='.$x.'">'.($x+1).'</a>';
+                        }
+                    }
+                    echo '</div>';
                 }
-                echo '</div>';
             ?>
         </div>
     </div>
